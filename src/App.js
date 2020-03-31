e7b02f7609a41176438b826b748be0aa22c5ced5
llaulier <lioraloche@yahoo.fr>
2020-03-31 11:10:50 +0200

    app & navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
+import './App.css';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div>
-
+      <Navbar />
+      <QuoteCard />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(){
+    return(
+    <nav className="Navbar">
+        <a href = 'https://fr.wikipedia.org/wiki/Les_Simpson' target = "_blank">Simpson's Wiki</a> 
+    </nav>
+    );
+}
+
+export default Navbar;
+
